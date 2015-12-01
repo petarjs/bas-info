ac5c40ddd66aeb4c9b78470049ba387c12da9365
Petar Slovic <petar.slovic@gmail.com>
2015-12-01 01:09:55 +0100

    working on datebpicker

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -6,8 +6,11 @@ import withStyles from '../../decorators/withStyles';
 import styles from './SearchPage.scss';
 import _ from 'lodash';
 import foldToAscii from 'fold-to-ascii';
+import moment from 'moment';
 
 const AutoComplete = require('material-ui/lib/auto-complete');
+const DropDownMenu = require('material-ui/lib/drop-down-menu');
+const DatePickerDialog = require('material-ui/lib/date-picker/date-picker-dialog');
 
 import placesJson from '../../data/places';
 
@@ -21,7 +24,23 @@ class SearchPage extends Component {
   };
 
   state = {
-    places: []
+    places: [],
+    dates: [{
+      payload: moment().format('MM/DD/YYYY'),
+      text: `${moment().format('DD/MM/YYYY')} - Danas`
+    }, {
+      payload: moment().add(1, 'days').format('MM/DD/YYYY'),
+      text: `${moment().add(1, 'days').format('DD/MM/YYYY')} - Sutra`
+    }, {
+      payload: moment().add(2, 'days').format('MM/DD/YYYY'),
+      text: `${moment().add(2, 'days').format('DD/MM/YYYY')}`
+    }, {
+      payload: moment().add(3, 'days').format('MM/DD/YYYY'),
+      text: `${moment().add(3, 'days').format('DD/MM/YYYY')}`
+    }, {
+      payload: 'custom',
+      text: `Izaberi`
+    }]
   };
 
   constructor(props) {
@@ -46,6 +65,18 @@ class SearchPage extends Component {
     console.log(r);
   }
 
+  onDateChange(e, selectedIndex, menuItem) {
+    console.log(menuItem);
+    if(menuItem.payload === 'custom') {
+      console.log('open datepicker');
+      this.refs.datePicker.show();
+    }
+  }
+
+  onDateAccept(date) {
+    console.log('chosen date', moment(date).format('DD/MM/YYYY'));
+  }
+
   render() {
     const title = 'Search';
     this.context.onSetTitle(title);
@@ -60,8 +91,20 @@ class SearchPage extends Component {
             dataSource= {this.state.places}
             onUpdateInput={this.onPlaceChange.bind(this)}
             filter={this.onPlaceFilter.bind(this)}
-            onNewRequest={this.onNewRequest.bind(this)}
-             />
+            onNewRequest={this.onNewRequest.bind(this)} />
+
+          <DropDownMenu
+            menuItems={this.state.dates}
+            onChange={this.onDateChange.bind(this)} />
+
+          <DatePickerDialog
+            ref='datePicker'
+            hintText=""
+            autoOk={true}
+            onAccept={this.onDateAccept.bind(this)}
+            // minDate={this.state.minDate}
+            // maxDate={this.state.maxDate}
+            showYearSelector={false} />
 
           <button onClick={this.onClickSearch.bind(this)}>Search</button>
           <p>...</p>
